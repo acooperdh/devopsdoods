e3b6fea63e34ead78e60cd69c3497b7221f4ca5d
acooperdh <an538926@dal.ca>
2022-03-03 15:05:25 -0400

    added chakra icons & navbar layout

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,21 @@
-// import { } from '@chakra-ui/react'
+import { Stack, Box, Button } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons';
 
+// NavBar needs 
+/* 
+- timeline button
+- create a blog 
+- profile (?)
+*/ 
 export default function NavBar() {
 	return (
-		<></>
+		<Box p='2' display='flex'>
+			<Stack direction='row' spacing={4}>
+				<Button>Timeline</Button> 
+				<Button colorScheme='green' rightIcon={<AddIcon />}>Create Blog</Button>
+				<Button>Profile</Button>
+				<Button colorScheme='red'>Logout</Button>
+			</Stack>
+		</Box> 
 	);
 }
