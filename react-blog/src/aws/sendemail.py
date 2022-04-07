48021ffed4fbb0e30c8b8648618ded8ba834a4f2
Drew <an538926@dal.ca>
2022-04-07 15:57:11 -0300

    small style change

--- react-blog/src/aws/sendemail.py
+++ react-blog/src/aws/sendemail.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     if (event["Records"]["status == 'SUCCESS'"]):
 
         message = "A new photo has been uploaded to S3, check it out here: " + \
-            event["Response"]["ObjectURL"]
+            event["Records"]["ObjectURL"]
         subject = "New Photo Uploaded"
     else:
         message = "There was an error uploading your photo to S3, please check the lambda logs to determine where the error is."
